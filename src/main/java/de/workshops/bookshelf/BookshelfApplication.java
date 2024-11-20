806e557c8f960983a4ac670a9237728b81b55c0d
Birgit Kratz <mail@birgitkratz.de>
2024-11-20 16:25:40 +0100

    Tag 1: initialize Application, Banner, Logback configuration

--- src/main/java/de/workshops/bookshelf/BookshelfApplication.java
+++ src/main/java/de/workshops/bookshelf/BookshelfApplication.java
@@ -1,10 +1,16 @@
 package de.workshops.bookshelf;
 
+import org.slf4j.MDC;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BookshelfApplication {
+
+	public BookshelfApplication() {
+		// MDC = Mapped Diagnostic Context
+		MDC.put("user", "Birgit");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookshelfApplication.class, args);
